1ce00fcc225d0dc19c06910a90ec9f2a2833e064
RedcDevHA <aunghansoe@yahoo.se>
2024-03-14 16:18:45 +0100

    Teori left

--- Lab_3_Source_Code/Lab_3_Source_Code/node.c
+++ Lab_3_Source_Code/Lab_3_Source_Code/node.c
@@ -1,11 +1,48 @@
 #include "node.h"
 
+void distribute_packet(struct rtpkt pkt) {
+	for (int i = 0; i < MAX_NODES; i++) {
+		if (i == pkt.sourceid) continue;
+        if ((pkt.sourceid % 2 == 1) && (i % 2 == 1)) continue;
+		pkt.destid = i;
+		tolayer2(pkt);
+	}
+}
+
 void rtinit(struct distance_table *table, int node) {
-	// Tables are already initialized
-	printdt(table, node);
+    struct rtpkt initial_broadcast = { .destid = INF, .sourceid = node };
+	for (int i = 0; i < MAX_NODES; i++) {
+		initial_broadcast.mincost[i] = table->costs[node][i];
+	}
+
+	distribute_packet(initial_broadcast);
+
+    if(TRACE >= 3)
+        printdt(table, node);
 }
 
 void rtupdate(struct distance_table *table, int node, struct rtpkt *pkt) {
-	printdt(table, node);
-}
+    int changed = 0;
 
+	for (int i = 0; i < MAX_NODES; i++) {
+		if ((pkt->mincost[i] + table->costs[node][pkt->sourceid]) < table->costs[pkt->sourceid][i]) {
+			table->costs[pkt->sourceid][i] = pkt->mincost[i] + table->costs[node][pkt->sourceid];
+			changed = 1;
+		}
+	}
+
+	if (changed) {
+		struct rtpkt upt_table = { .destid = INF, .sourceid = node };
+		for (int i = 0; i < MAX_NODES; i++) {
+			int best_route = table->costs[node][i];
+			for (int j = 0; j < MAX_NODES; j++) {
+				if (table->costs[j][i] < best_route)
+					best_route = table->costs[j][i];
+			}
+			upt_table.mincost[i] = best_route;
+		}
+		distribute_packet(upt_table);
+	}
+    if(TRACE >= 3)
+        printdt(table, node);
+}
--- Lab_3_Source_Code/Lab_3_Source_Code/node.h
+++ Lab_3_Source_Code/Lab_3_Source_Code/node.h
@@ -3,6 +3,10 @@
 
 #include "sim_engine.h"
 #include <stdio.h>
+#include <limits.h>
+#define MAX_NODES 4
+#define INF 999
+extern int TRACE;
 
 void rtinit(struct distance_table *table, int nodeno);
 void rtupdate(struct distance_table *table, int nodeno, struct rtpkt *pkt);
